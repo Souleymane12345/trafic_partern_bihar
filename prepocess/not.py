5ae5a6c39f679db916caabe55dee6e3c5c1d416d
Souley <62396414+Souleymane12345@users.noreply.github.com>
2024-04-03 23:13:30 +0200

    Update not.py

--- prepocess/not.py
+++ prepocess/not.py
@@ -44,3 +44,31 @@ class COCOSegmentationDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.ids)
+
+
+def get_transform():
+    custom_transforms = []
+    custom_transforms.append(torchvision.transforms.ToTensor())
+    return torchvision.transforms.Compose(custom_transforms)
+
+
+
+# create own Dataset
+my_dataset = myOwnDataset(root=train_data_dir,
+                          annotation=train_coco,
+                          transforms=get_transform()
+                          )
+
+# collate_fn needs for batch
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+# Batch size
+train_batch_size = 1
+
+# own DataLoader
+data_loader = torch.utils.data.DataLoader(my_dataset,
+                                          batch_size=train_batch_size,
+                                          shuffle=True,
+                                          num_workers=4,
+                                          collate_fn=collate_fn)
