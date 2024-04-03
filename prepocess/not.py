a0f4e9c66b403750631b8f68d17e1d003a7fc0eb
Souley <62396414+Souleymane12345@users.noreply.github.com>
2024-04-03 23:29:02 +0200

    Update not.py

--- prepocess/not.py
+++ prepocess/not.py
@@ -72,3 +72,12 @@ data_loader = torch.utils.data.DataLoader(my_dataset,
                                           shuffle=True,
                                           num_workers=4,
                                           collate_fn=collate_fn)
+
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# DataLoader is iterable over Dataset
+for imgs, annotations in data_loader:
+    imgs = list(img.to(device) for img in imgs)
+    annotations = [{k: v.to(device) for k, v in t.items()} for t in annotations]
+    print(annotations)
